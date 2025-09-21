32a59a786dfa9abf196c5ac3c6f4c6e5af0ae6eb
DEIVILO <ernestskrafts14@gmail.com>
2025-09-21 17:34:44 +0300

    added thank-you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,17 @@
+const page = () => {
+    return (
+        <div>
+            <h1>
+                TestStore
+            </h1>
+            <h1>
+                Thank u for your purchase, big dawg!
+            </h1>
+            <h2>
+                This is a tests Element to see what happens
+            </h2>
+        </div>
+    );
+}
+
+export default page;
